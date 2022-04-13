bcfdcebf2624c8624b6ae879ab641603bc113de0
RethyLogiscool <93673094+RethyLogiscool@users.noreply.github.com>
2022-04-13 12:01:54 +0200

    JSX pičo

--- ClientApp/src/components/App.js
+++ ClientApp/src/components/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router";
 import Layout from "./Layout";
 import Home from "./Home";
 import FetchData from "./FetchData/FetchData";
+import Overview from "./Overview/Overview";
 import "./App.css";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <Layout>
       <Route exact path="/" component={Home} />
       <Route path="/fetch-data" component={FetchData} />
+      <Route path="/overview" component={Overview} />
     </Layout>
   );
 };
---
+++ ClientApp/src/components/ProductList/ProductList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <>
+      <a href="">
+        <div>
+          <h2>PG24A</h2>
+          <span>Detail</span>
+        </div>
+      </a>
+    </>
+  );
+};
+
+export default ProductList;
