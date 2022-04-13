7b87baf761f5261b63046352a159b3c7e31972e6
Michael Polivka <michael.polivka1@gmail.com>
2022-04-13 13:29:33 +0200

    cau

--- ClientApp/src/components/ProductList/ProductList.js
+++ ClientApp/src/components/ProductList/ProductList.js
@@ -4,24 +4,28 @@ const ProductList = () => {
   const productNames = ['a', 'b'];
   const ProductCards = productNames.map((name) => (
     <a href="/overview" key={name}>
-      <div className="card mini-stat bg-primary">
-        <div className="card-body mini-stat-img">
-          <div className="mini-stat-icon">
-            <i className="mdi mdi-cube-outline float-end"></i>
-          </div>
-          <div className="text-white">
-            <h6 className="text-uppercase mb-3 font-size-16 text-white">
-              {name}
-            </h6>
-            <h2 className="mb-4 text-white">Detail</h2>
-            <span className="badge bg-info"> +11% </span>{' '}
-            <span class="ms-2">From previous period</span>
+      <div className="col-xl-3 col-sm-6">
+        <div className="card mini-stat bg-primary">
+          <div className="card-body mini-stat-img">
+            <div className="mini-stat-icon">
+              <i className="mdi mdi-cube-outline float-end"></i>
+            </div>
+            <div className="text-white">
+              <h6 className="text-uppercase mb-3 font-size-16 text-white">
+                Orders
+              </h6>
+              <h2 className="mb-4 text-white">{name}</h2>
+            </div>
           </div>
         </div>
       </div>
     </a>
   ));
-  return <>{ProductCards}</>;
+  return (
+    <>
+      <div className="row">{ProductCards}</div>
+    </>
+  );
 };
 
 export default ProductList;
