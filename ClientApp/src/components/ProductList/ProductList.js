bcd781ab8e19ecf4d113d5690be5b2fbcbf8dfe6
RethyLogiscool <93673094+RethyLogiscool@users.noreply.github.com>
2022-04-13 15:47:02 +0200

    Update ProductList.js

--- ClientApp/src/components/ProductList/ProductList.js
+++ ClientApp/src/components/ProductList/ProductList.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const ProductList = () => {
-  const productNames = ['a', 'b'];
+  const productNames = ["a", "b"];
   const ProductCards = productNames.map((name) => (
-    <a href="/overview" key={name}>
-      <div className="col-xl-3 col-sm-6">
+    <div className="col-xl-3 col-sm-6">
+      <a href="/overview" key={name}>
         <div className="card mini-stat bg-primary">
           <div className="card-body mini-stat-img">
             <div className="mini-stat-icon">
@@ -18,8 +18,8 @@ const ProductList = () => {
             </div>
           </div>
         </div>
-      </div>
-    </a>
+      </a>
+    </div>
   ));
   return (
     <>
