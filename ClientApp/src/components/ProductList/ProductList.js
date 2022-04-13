fd385cc26b6d34c30511f0c6d5e2aa45d1518b5b
Michael Polivka <michael.polivka1@gmail.com>
2022-04-13 13:15:35 +0200

    Update ProductList.js
    srandy

--- ClientApp/src/components/ProductList/ProductList.js
+++ ClientApp/src/components/ProductList/ProductList.js
@@ -3,10 +3,21 @@ import React from 'react';
 const ProductList = () => {
   const productNames = ['a', 'b'];
   const ProductCards = productNames.map((name) => (
-    <a href={`/${name}`} key={name}>
-      <div>
-        <h2>{name}</h2>
-        <span>Detail</span>
+    <a href="/overview" key={name}>
+      <div className="card mini-stat bg-primary">
+        <div className="card-body mini-stat-img">
+          <div className="mini-stat-icon">
+            <i className="mdi mdi-cube-outline float-end"></i>
+          </div>
+          <div className="text-white">
+            <h6 className="text-uppercase mb-3 font-size-16 text-white">
+              {name}
+            </h6>
+            <h2 className="mb-4 text-white">Detail</h2>
+            <span className="badge bg-info"> +11% </span>{' '}
+            <span class="ms-2">From previous period</span>
+          </div>
+        </div>
       </div>
     </a>
   ));
