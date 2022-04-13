97d48b140e3d7ce647983eae566663b0c85387e6
RethyLogiscool <93673094+RethyLogiscool@users.noreply.github.com>
2022-04-13 13:31:56 +0200

    Update Overview.js

--- ClientApp/src/components/Overview/Overview.js
+++ ClientApp/src/components/Overview/Overview.js
@@ -31,13 +31,11 @@ const Overview = () => {
       <div className="row">
         <h1>IL4 PG24A</h1>
       </div>
-      <div>
-        <h2>{`Chybovost produktu v datovém okně ${
-          date.startDate == null ? "" : date.startDate.toLocaleDateString()
-        } - ${
-          date.endDate == null ? "" : date.endDate.toLocaleDateString()
-        }`}</h2>
-        <div>
+      <div className="row">
+        <h2>Chybovost produktu</h2>
+      </div>
+      <div className="row">
+        <div className="col-md-6">
           <div>
             <C3Chart data={data} color={color} />
           </div>
@@ -51,25 +49,6 @@ const Overview = () => {
               direction="horizontal"
             />
             ;
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-xl-3 col-sm-6">
-          <div className="card mini-stat bg-primary">
-            <div className="card-body mini-stat-img">
-              <div className="mini-stat-icon">
-                <i className="mdi mdi-cube-outline float-end"></i>
-              </div>
-              <div className="text-white">
-                <h6 className="text-uppercase mb-3 font-size-16 text-white">
-                  Orders
-                </h6>
-                <h2 className="mb-4 text-white">1,587</h2>
-                <span className="badge bg-info"> +11% </span>{" "}
-                <span className="ms-2">From previous period</span>
-              </div>
-            </div>
           </div>
         </div>
       </div>
