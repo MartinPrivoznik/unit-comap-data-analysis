b9b63696c63f5a32ee436312bf3abf018cb963d5
PePe <35529745+petrpepe@users.noreply.github.com>
2022-04-13 15:47:22 +0200

    More fetching and prodcut list update

--- ClientApp/src/components/ProductList/ProductList.js
+++ ClientApp/src/components/ProductList/ProductList.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { getProductNames } from "../../actions/actions";
 
 const ProductList = () => {
-  const productNames = ['a', 'b'];
-  const ProductCards = productNames.map((name) => (
-    <a href="/overview" key={name}>
+  const [dataApi, setData] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getProductNames();
+      setData(data);
+    };
+
+    getData();
+  }, []);
+
+  const ProductCards = !dataApi ? (
+    <p>
+      <em>Loading...</em>
+      {/* Tady by to chtělo nějaký pěkný spinner :) https://www.w3schools.com/howto/howto_css_loader.asp */}
+    </p>
+  ) : (dataApi.map((el) => (
+    <a href="/overview" key={el.name}>
+      {console.log(dataApi)}
       <div className="col-xl-3 col-sm-6">
         <div className="card mini-stat bg-primary">
           <div className="card-body mini-stat-img">
@@ -14,13 +31,13 @@ const ProductList = () => {
               <h6 className="text-uppercase mb-3 font-size-16 text-white">
                 Orders
               </h6>
-              <h2 className="mb-4 text-white">{name}</h2>
+              <h2 className="mb-4 text-white">{el.name}</h2>
             </div>
           </div>
         </div>
       </div>
     </a>
-  ));
+  )));
   return (
     <>
       <div className="row">{ProductCards}</div>
